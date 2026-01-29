0f0871a6098a80510ceda7632602371be5f3e1fa
SONIcsez1234 <150399672+SONIcsez1234@users.noreply.github.com>
2026-01-29 12:49:47 -0500

    Proper GitHub Setup stuff
    Probably gonna have to number the folders later for propper compiling n stuff

---
+++ Virdis/LUA/LUA_COLORS.lua
@@ -0,0 +1,10 @@
+freeslot("SKINCOLOR_QUETZAL")
+skincolors[SKINCOLOR_QUETZAL] = {
+	name = "Quetzal",
+//	ramp = {113,113,114,115,103,125,125,126,126,127,108,109,139,139,29,31},(First old ass colorset)(Yes theres swears in here, yw anyone digging in here)
+	ramp = {0,120,99,101,101,101,103,126,143,138,138,174,175,175,169,187},
+	invcolor = SKINCOLOR_RUBY,
+	invshade = 0,
+	chatcolor = V_PERIDOTMAP,
+	accessible = true
+}
